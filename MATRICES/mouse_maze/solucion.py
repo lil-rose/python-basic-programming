2a5f5d99f7e18f11abc8c51d1a4e6936569311d1
lil'Rose <rosita.hormannlobos@gmail.com>
2019-07-10 16:20:16 -0400

    Fixed some logic in the solution of mouse maze

--- MATRICES/mouse_maze/solucion.py
+++ MATRICES/mouse_maze/solucion.py
@@ -1,7 +1,7 @@
 """
-SOLUCIÓN PROPUESTA EJERCICIO 1 GUÍA 10 TIPO PRUEBA- Tutoría de Programación 14-diciembre-2018
+SOLUCIÓN PROPUESTA PARA PROBLEMA DEL LABERINTO DEL RATÓN
 Autor: Rosita Hormann Lobos.
-Contacto: rosita.hormannlobos@gmail.com o rhl003@alumnos.ucn.cl
+Contacto: rosita.hormannlobos@gmail.com
 
 
 """
@@ -48,7 +48,7 @@ while seguir:
     expTxt = open(listaExperimentos[pos],"r") # Se abre el archivo del experimento seleccionado.
 
     ######################################################
-    #Se procede a procesar el archivo del laberinto
+    # Se procede a procesar el archivo del laberinto
     nombreLaberinto = expTxt.readline().strip() # Primera linea del archivo debe ser el nombre del archivo del laberinto
     mazeTxt = open(nombreLaberinto,"r") # Se abre el archivo del laberinto.
     linea = mazeTxt.readline().strip() # Se lee la primera linea, que tiene dos numeros en formato n,m
@@ -77,104 +77,104 @@ while seguir:
                 laberinto[i][j] = 5
                 cantidadSalidas += 1
             else:
-                print("El archivo tiene un error!")
                 archivoBueno = False
-    if archivoBueno:
-        if cantidadSalidas <= 0:
-            print("Experimento no válido (laberinto no tiene salidas)")
-            print(laberinto)
-            print(cantidadSalidas)
-        else:
-            ######################################################
-            # Ahora se posiciona al ratón en el laberinto.
-            #posicionInicial = expTxt.readline().strip().split(",") # La siguiente línea a leer es la posicion inicial del ratón
-            posicionInicial = expTxt.readline().strip()
-            print(posicionInicial)
-            posicionInicial = posicionInicial.split(",")
-            filaActual = int(posicionInicial[0]) # fila en la que se encuentra el ratón.
-            columnaActual = int(posicionInicial[1])  # columna en la que se encuentra el ratón.
-            print("POSICION INICIAL: ", posicionInicial)
-            laberinto[filaActual][columnaActual] = 6 # Se marca posicion del ratón
-            
-            separacion()
-            print(laberinto) # Se imprime el laberinto tal cual está ahora.
-            separacion()
-            ######################################################
+    if not archivoBueno:
+        print("El archivo tiene un error!")
+    elif cantidadSalidas <= 0:
+        print("Experimento no válido (laberinto no tiene salidas)")
+        print(laberinto)
+        print(cantidadSalidas)
+    else:
+        ######################################################
+        # Ahora se posiciona al ratón en el laberinto.
+        #posicionInicial = expTxt.readline().strip().split(",") # La siguiente línea a leer es la posicion inicial del ratón
+        posicionInicial = expTxt.readline().strip()
+        print(posicionInicial)
+        posicionInicial = posicionInicial.split(",")
+        filaActual = int(posicionInicial[0]) # fila en la que se encuentra el ratón.
+        columnaActual = int(posicionInicial[1])  # columna en la que se encuentra el ratón.
+        print("POSICION INICIAL: ", posicionInicial)
+        laberinto[filaActual][columnaActual] = 6 # Se marca posicion del ratón
+        
+        separacion()
+        print(laberinto) # Se imprime el laberinto tal cual está ahora.
+        separacion()
+        ######################################################
 
-            ######################################################
-            # Ahora comenzaremos a mover el ratón a través del laberinto.
-            movimiento = expTxt.readline().strip()
-            ratonSalio = False
-            quesosCapturados = 0
+        ######################################################
+        # Ahora comenzaremos a mover el ratón a través del laberinto.
+        movimiento = expTxt.readline().strip()
+        ratonSalio = False
+        quesosCapturados = 0
+        
+        while movimiento != "" and not ratonSalio:
+            x = input("Presione enter para continuar") # Esta línea de código nos permitirá que la lectura de los movimientos del ratón no se realicen todas de una, sino que el programa espere a que el usuario presione "enter" para pasar al siguiente movimiento.
+            separacion()
             
-            while movimiento != "" and not ratonSalio:
-                x = input("Presione enter para continuar") # Esta línea de código nos permitirá que la lectura de los movimientos del ratón no se realicen todas de una, sino que el programa espere a que el usuario presione "enter" para pasar al siguiente movimiento.
+            movFilas = 0
+            movColumnas = 0
+
+            if movimiento == "U": #up (arriba)
+                movFilas = -1 # Hay que disminuir la fila.
+                
+            elif movimiento == "D": #down (abajo)
+                movFilas = 1 # Hay que aumentar la fila.
+                
+            elif movimiento == "L": # left (izquierda)
+                movColumnas = -1 # Hay que disminuir la columna.
+                
+            elif movimiento == "R": # right (derecha)
+                movColumnas = 1 # Hay que aumentar la columna.
+
+            nuevaFila = filaActual + movFilas
+            nuevaColumna = columnaActual + movColumnas
+            
+            ######################################################
+            # Ahora analizaremos si el movimiento era válido o no.
+            if nuevaFila < 0 or nuevaFila >= n or nuevaColumna < 0 or nuevaColumna >= m: # Aquí corroboramos que no nos estamos saliendo de la matriz "laberinto"
+                print("Movimiento inválido!")
+            elif laberinto[nuevaFila][nuevaColumna] == 1: # PARED. Movimiento inválido.
+                print("Movimiento inválido! (había una pared)")
+            else:
+                ######################################################
+                # Ahora analizamos los casos de movimientos válidos
+                
+                if laberinto[nuevaFila][nuevaColumna] == 8: # QUESO. Captura el queso.
+                    print("Queso capturado!")
+                    quesosCapturados+=1
+                elif laberinto[nuevaFila][nuevaColumna] == 5: # SALIDA.
+                    ratonSalio = True # Una vez el ratón sale no hay que seguir leyendo el archivo.
+                
+
+                ######################################################
+                # Se procede a marcar el movimiento en el laberinto
+                laberinto[filaActual][columnaActual] = 2 #Dejar huellas
+
+                filaActual = nuevaFila # Actualizar fila
+                columnaActual = nuevaColumna # Actualizar columna
+
+                laberinto[filaActual][columnaActual] = 6 # Actualizamos posicion del ratón en el laberinto.
+                
+                print(laberinto) # Finalmente imprimimos el laberinto
                 separacion()
                 
-                movFilas = 0
-                movColumnas = 0
-
-                if movimiento == "U": #up (arriba)
-                    movFilas = -1 # Hay que disminuir la fila.
-                    
-                elif movimiento == "D": #down (abajo)
-                    movFilas = 1 # Hay que aumentar la fila.
-                    
-                elif movimiento == "L": # left (izquierda)
-                    movColumnas = -1 # Hay que disminuir la columna.
-                    
-                elif movimiento == "R": # right (derecha)
-                    movColumnas = 1 # Hay que aumentar la columna.
-
-                nuevaFila = filaActual + movFilas
-                nuevaColumna = columnaActual + movColumnas
-                
-                ######################################################
-                # Ahora analizaremos si el movimiento era válido o no.
-                if nuevaFila < 0 or nuevaFila >= n or nuevaColumna < 0 or nuevaColumna >= m: # Aquí corroboramos que no nos estamos saliendo de la matriz "laberinto"
-                    print("Movimiento inválido!")
-                elif laberinto[nuevaFila][nuevaColumna] == 1: # PARED. Movimiento inválido.
-                    print("Movimiento inválido! (había una pared)")
-                else:
-                    ######################################################
-                    # Ahora analizamos los casos de movimientos válidos
-                    
-                    if laberinto[nuevaFila][nuevaColumna] == 8: # QUESO. Captura el queso.
-                        print("Queso capturado!")
-                        quesosCapturados+=1
-                    elif laberinto[nuevaFila][nuevaColumna] == 5: # SALIDA.
-                        ratonSalio = True # Una vez el ratón sale no hay que seguir leyendo el archivo.
-                    
-
-                    ######################################################
-                    # Se procede a marcar el movimiento en el laberinto
-                    laberinto[filaActual][columnaActual] = 2 #Dejar huellas
-
-                    filaActual = nuevaFila # Actualizar fila
-                    columnaActual = nuevaColumna # Actualizar columna
-
-                    laberinto[filaActual][columnaActual] = 6 # Actualizamos posicion del ratón en el laberinto.
-                    
-                    print(laberinto) # Finalmente imprimimos el laberinto
-                    separacion()
-                    
-                movimiento = expTxt.readline().strip() # Leemos siguiente movimiento
-            separacion()
-            print("FIN DEL EXPERIMENTO")
-            separacion()
-            ######################################################
-            #Ahora que terminamos de mover al ratón, hay que informar quesos capturados y si el ratón salió del laberinto o no.
-            print("El ratón capturó ", quesosCapturados, " de ", cantidadQuesos, " totales.")
-            if ratonSalio:
-                print("El ratón logró salir del laberinto!")
-            else:
-                print("El ratón no logró salir del laberinto...")
-            separacion()
+            movimiento = expTxt.readline().strip() # Leemos siguiente movimiento
+        separacion()
+        print("FIN DEL EXPERIMENTO")
+        separacion()
+        ######################################################
+        # Ahora que terminamos de mover al ratón, hay que informar quesos capturados y si el ratón salió del laberinto o no.
+        print("El ratón capturó ", quesosCapturados, " de ", cantidadQuesos, " totales.")
+        if ratonSalio:
+            print("El ratón logró salir del laberinto!")
+        else:
+            print("El ratón no logró salir del laberinto...")
+        separacion()
 
     ######################################################
     # Finalmente hay que preguntar al usuario si desea seguir consultando por experimentos
     respuesta = -1
-    while respuesta == -1: 
+    while respuesta != 0 and respuesta != 1: 
         respuesta = int(input("Desea consultar otro experimento? [0] NO, [1] Si"))
         if respuesta != 1 and respuesta != 0:
             print("Opción inválida!")
